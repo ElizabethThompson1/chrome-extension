c35688a3368e5f65cb009c07a76f4304163fffe7
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-08-27 16:21:04 -0500

    created a addevent listener that when i you typed in your lead then you could save it and it would render all saved items and then i creaded a dblclick to be able to delete the array on leads, i saved them on local storage

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-let myLeads = [];
+let myLeads = null
+myLeads = [];
 const inputBtn = document.getElementById("button-el");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
 });
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
 
 function renderLeads() {
   let listItems = "";
